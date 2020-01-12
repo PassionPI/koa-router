e0e9a29f55985d20ec022db1aab08914c3070b60
PassionPI <472121553@qq.com>
2020-01-12 20:46:33 +0800

    npm

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp')
 const ts = require('gulp-typescript')
 const del = require('del')
 
-const delPrevFile = () => del(['dist/**/*'])
+const delPrevFile = () => del(['lib/**/*'])
 
 const parseTs = () => src('src/*.ts')
   .pipe(ts())
-  .pipe(dest('dist/'))
+  .pipe(dest('lib/'))
 
   
 watch('src/*.ts', { delay: 1000 }, cb => {
